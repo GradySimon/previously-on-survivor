ff4c5347c6ee3aa3f4a48413c8e65990b01e3f0a
Grady Simon <grady@gradysimon.com>
2015-07-06 16:03:09 -0700

    Most major endpoints required for integration with Fantasy Survivor complete.

--- src/previously_on_survivor/core.clj
+++ src/previously_on_survivor/core.clj
@@ -23,13 +23,13 @@
 @(d/transact conn initial-data-tx)
 
 (defn value-tranformer [key datomic-value]
-  (cond (= key :common/uuid) (str datomic-value)
-        (= key :episode/air-date) (str datomic-value)
-        :else datomic-value))
+  (case key :common/uuid (str datomic-value)
+            :episode/air-date (str datomic-value)
+            datomic-value))
 
 (defn key-transformer [key]
-  (cond (= key :season/_episodes) "season"
-        :else (name key)))
+  (case key :season/_episodes "season"
+             (name key)))
 
 (defn to-json [datomic-data]
   (json/write-str datomic-data
@@ -37,9 +37,30 @@
                   :value-fn value-tranformer))
 
 (def episode-pull-spec
-  '[:common/uuid :episode/number :episode/air-date {:season/_episodes [:common/uuid :season/number]}])
+  [:common/uuid
+   :episode/number
+   :episode/air-date
+   {:season/_episodes [:common/uuid :season/number]}
+   :episode/events])
 
-(defn last-episode [_]
+(def player-pull-spec
+  [:common/uuid
+   :player/first-name
+   :player/last-name
+   :player/tribe])
+
+(def tribe-pull-spec
+  [:common/uuid
+   :tribe/name
+   {:player/_tribe player-pull-spec}])
+
+(def season-pull-spec
+  [:common/uuid
+   :season/number
+   {:season/episodes episode-pull-spec}
+   {:season/tribes tribe-pull-spec}])
+
+(defn last-episode []
   (let [db (d/db conn)]
     (->>
       (q '[:find (max ?air-date) .
@@ -49,42 +70,83 @@
       (q '[:find ?episode .
            :in $ ?last-air-date
            :where [?episode :episode/air-date ?last-air-date]]
-         db)
-      (d/pull db episode-pull-spec)
-      (to-json))))
+         db))))
 
-(defn next-episode [_]
+(defn next-episode []
   (let [db (d/db conn)]
     (->>
       (q '[:find (min ?air-date) .
            :where [_ :episode/air-date ?air-date]
-                  [(> ?air-date (java.util.Date.))]]
+                  [(>= ?air-date (java.util.Date.))]]
           db)
       (q '[:find ?episode .
            :in $ ?next-air-date
            :where [?episode :episode/air-date ?next-air-date]]
-          db)
-      (d/pull db episode-pull-spec)
-      (to-json))))
+          db))))
 
-(defn episode [episode-uuid]
+(defn current-season [] 
   (let [db (d/db conn)]
-    (->> [:common/uuid (java.util.UUID/fromString episode-uuid)]
-         (d/pull db episode-pull-spec)
-         (to-json))))
+    (q '[:find ?season .
+         :where [?season :common/designation :common.designation/current-season]]
+       db)))
 
-(defresource episode-by-uuid [uuid]
+(defn alive-players []
+  (let [db (d/db conn)]
+    (q '[:find [?player ...]
+         :in $ ?current-season
+         :where [?player :player/tribe ?tribe]
+                [?current-season :season/tribes ?tribe]
+                (not-join [?player]
+                  [?current-season :season/episodes ?episode]
+                  [?epsiode :episode/events ?event]
+                  [?event :event.tribal/voted-out ?player])]
+        db (current-season))))
+
+(defn detail
+  ([entity pull-spec]
+    (let [db (d/db conn)]
+      (d/pull db pull-spec entity)))
+  ([identity-mode entity pull-spec]
+    (let [db (d/db conn)]
+      (case identity-mode
+            :uuid (detail [:common/uuid (java.util.UUID/fromString entity)] pull-spec)
+            :eid (detail entity pull-spec)))))
+
+(defn detail-list [entities pull-spec]
+  (let [db (d/db conn)]
+    (d/pull-many db pull-spec entities)))
+
+(defresource last-episode-resource []
   :available-media-types ["application/json"]
-  :handle-ok (fn [_] (episode uuid)))
+  :handle-ok (fn [_] (to-json (detail (last-episode) episode-pull-spec))))
+
+(defresource next-episode-resource []
+  :available-media-types ["application/json"]
+  :handle-ok (fn [_] (to-json (detail (next-episode) episode-pull-spec))))
+
+(defresource current-season-resource []
+  :available-media-types ["application/json"]
+  :handle-ok (fn [_] (to-json (detail (current-season) season-pull-spec ))))
+
+(defresource episode-resource [uuid]
+  :available-media-types ["application/json"]
+  :handle-ok (fn [_] (to-json (detail :uuid uuid episode-pull-spec))))
+
+(defresource season-resource [uuid]
+  :available-media-types ["application/json"]
+  :handle-ok (fn [_] (to-json (detail :uuid uuid season-pull-spec))))
+
+(defresource alive-players-resource []
+  :available-media-types ["application/json"]
+  :handle-ok (fn [_] (to-json (detail-list (alive-players) player-pull-spec))))
 
 (defroutes app
-  (ANY "/next-episode" [] (resource
-                            :available-media-types ["application/json"]
-                            :handle-ok next-episode))
-  (ANY "/last-episode" [] (resource
-                            :available-media-types ["application/json"]
-                            :handle-ok last-episode))
-  (ANY "/episode/:uuid" [uuid] (episode-by-uuid uuid)))
+  (ANY "/last-episode" [] (last-episode-resource))
+  (ANY "/next-episode" [] (next-episode-resource))
+  (ANY "/episode/:uuid" [uuid] (episode-resource uuid))
+  (ANY "/current-season" [] (current-season-resource))
+  (ANY "/season/:uuid" [uuid] (season-resource uuid))
+  (ANY "/alive-players" [] (alive-players-resource)))
 
 (def handler
   (-> app
